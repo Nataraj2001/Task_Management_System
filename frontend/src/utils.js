e4e18ebfd5bfcf761559f502b4797d779b286e8d
Nataraj S Lakkundi <106070514+Nataraj2001@users.noreply.github.com>
2024-10-31 09:27:37 +0530

    Update utils.js

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -4,4 +4,4 @@ export const notify = (message, type) => {
     toast[type](message);
 }
 
-export const API_URL = 'https://task-management-system-api-nine.vercel.app/';
+export const API_URL = 'https://task-management-system-api-nine.vercel.app';
